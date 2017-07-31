4827a5755756124960cc66ebaf2b1c29ed8f9789
Nick Jalbert <nickjalbert@gmail.com>
2017-07-30 21:12:45 -0700

    port tests for utilities

---
+++ backgammon/test.py
@@ -0,0 +1,79 @@
+import unittest
+from utility import (is_valid_board, get_blank_board, white_wins, 
+                     black_wins, get_initial_board, roll_dice, 
+                     black_can_bear_off, white_can_bear_off, 
+                     black_position_is_outer, white_position_is_outer,
+                     BLACK_OFF_INDEX, WHITE_OFF_INDEX)
+
+class TestUtilities(unittest.TestCase):
+    def test_get_blank_board(self):
+        board = list(get_blank_board())
+        board[0] = (15,0)
+        board[1] = (0,15)
+        self.assertTrue(is_valid_board(board))
+        board = list(get_blank_board())
+        board[0] = (15,15)
+        self.assertFalse(is_valid_board(board))
+
+    def test_get_initial_board(self):
+        self.assertTrue(is_valid_board(get_initial_board()))
+
+    def test_black_wins(self):
+        self.assertFalse(black_wins(get_initial_board()))
+        board = list(get_blank_board())
+        board[BLACK_OFF_INDEX] = (15, 0)
+        board[1] = (0, 15)
+        self.assertTrue(black_wins(board))
+        self.assertFalse(white_wins(board))
+
+    def test_white_wins(self):
+        self.assertFalse(white_wins(get_initial_board()))
+        board = list(get_blank_board())
+        board[WHITE_OFF_INDEX] = (0, 15)
+        board[1] = (15, 0)
+        self.assertTrue(white_wins(board))
+        self.assertFalse(black_wins(board))
+
+    def test_roll_dice(self):
+        self.assertIsNotNone(roll_dice())
+
+    def test_black_can_bear_off(self):
+        board = list(get_blank_board())
+        board[10] = (15,0)
+        board[1] = (0,15)
+        self.assertTrue(is_valid_board(board))
+        self.assertFalse(black_can_bear_off(board))
+        board[10] = (0,0)
+        board[0] = (15,0)
+        self.assertTrue(black_can_bear_off(board))
+
+    def test_white_can_bear_off(self):
+        board = list(get_blank_board())
+        board[10] = (15,0)
+        board[15] = (0,15)
+        self.assertTrue(is_valid_board(board))
+        self.assertFalse(white_can_bear_off(board))
+        board[15] = (0,0)
+        board[23] = (0,15)
+        self.assertTrue(white_can_bear_off(board))
+
+    def test_black_position_is_outer(self):
+        board = list(get_blank_board())
+        board[10] = (12,0)
+        board[9] = (3,0)
+        board[1] = (0,15)
+        self.assertTrue(is_valid_board(board))
+        self.assertFalse(black_position_is_outer(board, 9))
+        self.assertTrue(black_position_is_outer(board, 10))
+
+    def test_white_position_is_outer(self):
+        board = list(get_blank_board())
+        board[10] = (15,0)
+        board[15] = (0,13)
+        board[16] = (0,2)
+        self.assertTrue(is_valid_board(board))
+        self.assertFalse(white_position_is_outer(board, 16))
+        self.assertTrue(white_position_is_outer(board, 15))
+
+if __name__ == '__main__':
+    unittest.main()
--- backgammon/utility.py
+++ backgammon/utility.py
@@ -1,39 +1,3 @@
-"""
-We represent the backgammon board as a 28 element list of 2-tuples.  List
-indices 0 through 23 represent each point on the board.  The first element of
-each 2-tuple is the number of black pieces on the point.  The second element
-of each 2-tuple is the number of white pieces on the point.
-
-List index 24 is the tuple representing the bar where black pieces go when
-hit.
-
-List index 25 is the tuple representing the bar where white pieces go when
-hit.
-
-List index 26 is the tuple representing the the area where born off black
-pieces reside.
-
-List index 27 is the tuple representing the the area where born off white
-pieces reside.
-
-White moves counter-clockwise and bears off to the bottom right.  Black moves
-clockwise and bears off to the top right.
-
-
-Illustration of the list indices and their correspondence to the board:
-
- |  1  1  9  8  7  6  |   |  5  4  3  2  1  0  | Black Off: 26
- |  1  0              |   |                    | Black Bar: 24
- |                    |   |                    |
- |  _  _  _  _  _  _  |   |  _  _  _  _  _  _  |
- |                    |   |                    |
- |                    |   |                    |
- |  1  1  1  1  1  1  |   |  1  1  2  2  2  2  |
- |  2  3  4  5  6  7  |   |  8  9  0  1  2  3  | White Off: 27
- |  _  _  _  _  _  _  |   |  _  _  _  _  _  _  | White Bar: 25
- |  .  .  .  .  .  .  |   |  .  .  .  .  .  .  |
-"""
-
 import random
 
 BLACK_INDEX = 0
@@ -44,6 +8,9 @@ BLACK_OFF_INDEX = 26
 WHITE_OFF_INDEX = 27
 
 def get_blank_board():
+    """
+    See README for explanation of the board data structure.
+    """
     return ((0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
             (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
             (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
